68fe7d68f00a7124e2fbaead8b5a69befec026ec
tch1001 <tanchienhao@gmail.com>
2025-11-02 21:36:00 +0800

    Fixed checkstyle

--- src/main/java/seedu/address/logic/commands/CompatibleCommand.java
+++ src/main/java/seedu/address/logic/commands/CompatibleCommand.java
@@ -36,7 +36,7 @@ public class CompatibleCommand extends Command {
                     "compatible recipients for donor " + predicate.getDonorBloodType()));
         }
         return new CommandResult(
-                String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, 
+                String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW,
                     "for donor " + predicate.getDonorBloodType(), model.getFilteredPersonList().size()));
     }
 
